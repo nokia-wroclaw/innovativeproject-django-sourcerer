d2a90a0d23be06752385424641164e668ca7db39
Cem Arslan <cemarslan@MacBook-Pro-2.local>
2020-03-30 11:47:56 +0200

    tested import_csv_data.py

--- source/tests/test_import_csv_data.py
+++ source/tests/test_import_csv_data.py
@@ -1,15 +1,35 @@
 import unittest
-from unittest.mock import MagicMock, Mock
+from unittest.mock import MagicMock
 import requests
 import requests_mock
 
+from source.django_sourcerer.domain.import_datas.import_csv_data import HandleCsvData
 
-class TestIntermediateLayer(unittest.TestCase):
+endpoint = 'https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv'
+column = ['Rank']
+
+
+class TestImportCsvData(unittest.TestCase):
     def test_requests(self):
         session = requests.Session()
         adapter = requests_mock.Adapter()
         session.mount('mock', adapter)
+        adapter.register_uri('GET', endpoint)
+        resp = session.get(endpoint)
 
-        adapter.register_uri('GET', 'https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv')
-        resp = session.get('https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv')
         assert resp.status_code == 200
+
+    def test_raise_error_condition(self):
+        HandleCsvData(endpoint, column).import_data()
+        checking_call_error = requests.exceptions.ConnectionError()
+        checking_call_error = MagicMock()
+
+        checking_call_error.assert_not_called()
+
+    def test_import_data_method_return(self):
+        return_value = HandleCsvData(endpoint, column).import_data()
+        self.assertFalse(return_value is None)
+
+
+if __name__ == "__main__":
+    unittest.main()
